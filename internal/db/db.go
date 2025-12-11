d2ac3f9e7b04ac7a569bbb25d732fea569c56d81
ssoyeonisgood <haikyu5123@gmail.com>
2025-12-11 23:13:08 +0800

    Add .env file

--- internal/db/db.go
+++ internal/db/db.go
@@ -3,23 +3,29 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
 
 func Connect() error {
-	var err error
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, reading environment variables directly")
+	}
 
-	host := "localhost"
-	port := 5432
-	user := "soyeon_go"
-	password := "soyeon_go"
-	dbname := "studentdb"
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
 
 	dsn := fmt.Sprintf(
-		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname,
 	)
 
